dab10074ac6f4fde6e2485de30f4b59478ef6a33
lo568los <soumyadeepsarma3@gmail.com>
2024-11-25 23:05:39 +0530

    final data

--- scaled_codes/Launch.sh
+++ scaled_codes/Launch.sh
@@ -4,7 +4,7 @@ for Nloop in 6
 do
     for theta_loop in 1.07 
     do
-        for thetak_loop in 0 0.12 0.22 0.32 0.42 0.45 0.52 0.55 0.62 0.65 0.72 0.82
+        for thetak_loop in 0.42 0.45 0.52 0.55 0.62 0.65 0.72 0.82
         do
             
 
